cc32cca2160e38d4378ebeefb6f4957c2b05bdbb
kelvinrandu <randukelvin@gmail.com>
2024-02-16 15:03:59 +0300

    added dynamic category page

--- wapi/src/app/server-sitemap.xml/route.js
+++ wapi/src/app/server-sitemap.xml/route.js
@@ -7,7 +7,7 @@ const URL = "https://www.wapi.ke";
 function generateSiteMap(_items,_categories) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
+     <!--Wapiii sitemap-->
      <url>
        <loc>${URL}</loc>
      </url>
